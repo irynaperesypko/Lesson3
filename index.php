d800de5d4b712a399e6cdf12248a8dda41291aef
irynaperesypko <peresypko.irina@gmail.com>
2021-07-28 23:37:11 +0300

    lesson5

--- index.php
+++ index.php
@@ -1,78 +1,52 @@
 <?php
+echo 'Создать функцию определяющую какой тип данных ей передан и выводящей соответствующее сообщение' . "<br>";
+function showType($value)
+{
+    return 'Type value is ' . gettype($value);
+}
 
-$firstArr = [
-    'one' => 1,
-    'two' => [
-        'one' => 1,
-        'seven' => 22,
-        'three' => 32,
-    ],
-    'three' => [
-        'one' => 1,
-        'two' => 2,
-    ],
-    'foure' => 5,
-    'five' => [
-        'three' => 32,
-        'foure' => 5,
-        'five' => 12,
-        'six' => [
-            [
-                'three' => 32,
-                'foure' => 5,
-                'five' => 12,
-                'seven' => [
-                    [
-                        'three' => 32,
-                        'foure' => 5,
-                        'five' => 12,
-                    ]
-                ]]
-        ],
-    ],
-    'ten' => [
-        'three' => 32,
-        'foure' => 5,
-        'five' => 12,
-        'six' => [
-            [
-                'three' => 32,
-                'foure' => 5,
-                'five' => 12,
-                'seven' => [
-                    [
-                        'three' => 32,
-                        'foure' => 5,
-                        'five' => 12,
-                    ]
-                ]]
-        ],
-    ]
-];
+echo showType(5)
+    . "<br>";
 
-echo 'получить общее количество элементов в массиве ';
+echo 'Создать функцию которая считает все буквы b в переданной строке, в случае если передается не строка 
+функция должна возвращать false' . "<br>";
+function countLetter($str)
+{
+    if (!is_string($str)) {
+        return false;
+    }
+    return substr_count($str, 'b');
 
-function countItems(array $array)
+}
+
+echo countLetter('bbbrrr');
+
+function countSum(array $array)
 {
     $count = 0;
-    foreach ($array as $value) {
-        if (is_array($value)) {
-            $count += countItems($value);
+    foreach ($array as $item) {
+
+        if (is_array($item)) {
+            $count += countSum($item);
         } else {
-            $count++;
+            $count += $item;
         }
     }
     return $count;
 }
 
-$res = countItems($firstArr);
-echo $res;
+echo "<br>Создать функцию которая считает сумму значений всех элементов массива произвольной глубины <br>";
+$arr = [6, 6, 5, [4, 4, 4, [4, 4, 2, [50]]]];
+echo countSum($arr);
+echo "<br>Создать функцию которая определит сколько квадратов меньшего размера можно вписать в квадрат 
+большего размера размер возвращать в float<br>";
 
+function occurrence($a, $b): float
+{
+    return ($a*$a)/($b*$b);
+}
 
-
-
-
-
+echo occurrence(5.0,2);
 
 
 
